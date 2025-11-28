1cab5870576a29346eff0d5dc183116ba86e8659
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-11-28 10:49:21 +0100

    Try to reproduce issue 4657

---
+++ test/test-applications/integrations/TestApplication.NoCode/Class1.cs
@@ -0,0 +1,12 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace ClassLibrary1.Net8;
+
+public class Class1
+{
+    public void Do()
+    {
+        Console.WriteLine("Class1.Do");
+    }
+}
--- test/test-applications/integrations/TestApplication.NoCode/Program.cs
+++ test/test-applications/integrations/TestApplication.NoCode/Program.cs
@@ -3,69 +3,7 @@
 
 using TestApplication.Shared;
 
-namespace TestApplication.NoCode;
+ConsoleHelper.WriteSplashScreen(args);
 
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
-        ConsoleHelper.WriteSplashScreen(args);
-
-        var noCodeTestingClass = new NoCodeTestingClass();
-
-        noCodeTestingClass.TestMethod();
-        noCodeTestingClass.TestMethodA();
-        noCodeTestingClass.TestMethod(string.Empty);
-        noCodeTestingClass.TestMethod(int.MinValue);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        noCodeTestingClass.TestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-
-        NoCodeTestingClass.TestMethodStatic();
-
-        _ = noCodeTestingClass.ReturningTestMethod();
-        _ = noCodeTestingClass.ReturningStringTestMethod();
-        _ = noCodeTestingClass.ReturningCustomClassTestMethod();
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(int.MinValue);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        _ = noCodeTestingClass.ReturningTestMethod(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-
-        _ = NoCodeTestingClass.ReturningTestMethodStatic();
-
-        await noCodeTestingClass.TestMethodAsync();
-        await noCodeTestingClass.TestMethodAAsync();
-        await noCodeTestingClass.TestMethodAsync(string.Empty);
-        await noCodeTestingClass.TestMethodAsync(int.MinValue);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        await noCodeTestingClass.TestMethodAsync(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-
-        await NoCodeTestingClass.TestMethodStaticAsync();
-
-        _ = await noCodeTestingClass.IntTaskTestMethodAsync();
-#if NET
-        await noCodeTestingClass.ValueTaskTestMethodAsync();
-        _ = await noCodeTestingClass.IntValueTaskTestMethodAsync();
-#endif
-
-        _ = noCodeTestingClass.GenericTestMethod<int>();
-        _ = await noCodeTestingClass.GenericTestMethodAsync<int>();
-    }
-}
+var testClass = new ClassLibrary1.Net8.Class1();
+testClass.Do();
